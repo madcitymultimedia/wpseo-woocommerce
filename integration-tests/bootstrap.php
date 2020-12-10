5705d1b5d9d4ddf8bc58c19d53f04f2a501758c8
jrfnl <jrfnl@users.noreply.github.com>
2020-12-10 11:51:51 +0100

    Bootstrap: use a different pattern to load the plugins
    This should hopefully prevent a double include of the Composer autoloader which led to fatal "Cannot redeclare composerRequire...." errors.

--- integration-tests/bootstrap.php
+++ integration-tests/bootstrap.php
@@ -18,19 +18,53 @@ echo 'Version: 1.0' . PHP_EOL . PHP_EOL;
 if ( getenv( 'WP_PLUGIN_DIR' ) !== false ) {
 	define( 'WP_PLUGIN_DIR', getenv( 'WP_PLUGIN_DIR' ) );
 }
-
-$GLOBALS['wp_tests_options'] = [
-	'active_plugins' => [ 'wpseo-woocommerce/wpseo-woocommerce.php', 'wordpress-seo/wp-seo.php' ],
-];
+else {
+	define( 'WP_PLUGIN_DIR', dirname( dirname( __DIR__ ) ) );
+}
 
 require_once dirname( __DIR__ ) . '/vendor/yoast/wp-test-utils/src/WPIntegration/bootstrap-functions.php';
+
+// Determine the WP_TEST_DIR.
+$_tests_dir = WPIntegration\get_path_to_wp_test_dir();
+
+// Give access to tests_add_filter() function.
+require_once rtrim( $_tests_dir, '/' ) . '/includes/functions.php';
+
+/**
+ * Manually load the plugin being tested.
+ */
+$yoast_woocommerce_load_plugins = static function () {
+	require_once WP_PLUGIN_DIR . '/wordpress-seo/wp-seo.php';
+	require_once dirname( __DIR__ ) . '/wpseo-woocommerce.php';
+};
+
+/**
+ * Filter the plugins URL to pretend the plugin is installed in the test environment.
+ *
+ * @param string $url    The complete URL to the plugins directory including scheme and path.
+ * @param string $path   Path relative to the URL to the plugins directory. Blank string
+ *                       if no path is specified.
+ * @param string $plugin The plugin file path to be relative to. Blank string if no plugin
+ *                       is specified.
+ *
+ * @return string
+ */
+$yoast_woocommerce_filter_plugin_path = static function ( $url, $path, $plugin ) {
+	$plugin_dir = dirname( __DIR__ );
+	if ( $plugin === $plugin_dir . '/wpseo-woocommerce.php' ) {
+		$url = str_replace( dirname( $plugin_dir ), '', $url );
+	}
+
+	return $url;
+};
+
+// Add plugin to active mu-plugins - to make sure it gets loaded.
+tests_add_filter( 'muplugins_loaded', $yoast_woocommerce_load_plugins );
+
+// Overwrite the plugin URL to not include the full path.
+tests_add_filter( 'plugins_url', $yoast_woocommerce_filter_plugin_path, 10, 3 );
 
 /*
  * Load WordPress, which will load the Composer autoload file, and load the MockObject autoloader after that.
  */
 WPIntegration\bootstrap_it();
-
-if ( ! defined( 'WP_PLUGIN_DIR' ) || file_exists( WP_PLUGIN_DIR . '/wpseo-woocommerce/wpseo-woocommerce.php' ) === false ) {
-	echo PHP_EOL, 'ERROR: Please check whether the WP_PLUGIN_DIR environment variable is set and set to the correct value. The unit test suite won\'t be able to run without it.', PHP_EOL;
-	exit( 1 );
-}
