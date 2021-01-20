f3fe0c9715892177a499084532f9e97b43d3fa5e
Hans-Christiaan <hans-christiaan@yoast.com>
2021-01-20 14:09:30 +0100

    Do not replace newlines with tabs.

--- classes/presenters/woocommerce-schema-presenter.php
+++ classes/presenters/woocommerce-schema-presenter.php
@@ -62,7 +62,6 @@ class WPSEO_WooCommerce_Schema_Presenter extends Abstract_Indexable_Presenter {
 		$classes_string = \implode( ' ', $this->classes );
 
 		$output = \WPSEO_Utils::format_json_encode( $schema );
-		$output = \str_replace( "\n", \PHP_EOL . "\t", $output );
 		return '<script type="application/ld+json" class="' . \esc_attr( $classes_string ) . '">' . $output . '</script>' . PHP_EOL;
 	}
 
