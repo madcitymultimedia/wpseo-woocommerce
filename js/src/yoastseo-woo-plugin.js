6d85de5066dad0c2e82b1771edcb0cd25b7dba02
marijnyoast <marijn@yoast.com>
2021-03-24 21:12:05 +0100

    remove unused code

--- js/src/yoastseo-woo-plugin.js
+++ js/src/yoastseo-woo-plugin.js
@@ -1,7 +1,6 @@
 /* global YoastSEO, wpseoWooL10n, tinyMCE */
 
 import { getExcerpt, addExcerptEventHandlers, isTinyMCEAvailable } from "./yoastseo-woo-handle-excerpt-editors";
-import { addFilter } from "@wordpress/hooks";
 import { dispatch } from "@wordpress/data";
 
 const PLUGIN_NAME = "YoastWooCommerce";
