d39cccae5ae608ef0581dd5f4f9b7df417f7d7d0
noud-github <arnoud@yoast.com>
2021-05-25 09:52:53 +0200

    fix regexp

--- config/grunt/task-config/update-changelog-with-latest-pr-texts.js
+++ config/grunt/task-config/update-changelog-with-latest-pr-texts.js
@@ -2,17 +2,16 @@
 /* eslint-disable no-useless-escape */
 /* eslint-disable max-len */
 // Custom task
-// Rem defaultChangelogEntrys: "Other:\n* Includes every change in Yoast SEO core " + "VERSIONNUMBER" + ". See the [core changelog](https://wordpress.org/plugins/wordpress-seo/#developers).\n",
 
 module.exports = {
 	"wpseo-woocommerce": {
 		options: {
-			// Premium header:
+			// Woocommerce header:
 			// ### 15.9: February 23rd, 2021
 			readmeFile: "./README.md",
 			releaseInChangelog: /[#] \d+\.\d+(\.\d+)?\: /g,
-			matchChangelogHeader: /Changelog \n\n/ig,
-			newHeadertemplate: "### " + "VERSIONNUMBER" + ": " + "DATESTRING"  + "\n",
+			matchChangelogHeader: /Changelog\n=========\n\n/ig,
+			newHeadertemplate: "Changelog\n=========\n\n" + "### " + "VERSIONNUMBER" + ": " + "DATESTRING"  + "\n",
 			matchCorrectLines: "### " + "VERSIONNUMBER" + "(.|\\n)*?(?=(### \\d+[\.\\d]+\: |$))",
 			matchCorrectHeader: "### " + "VERSIONNUMBER" + "(.|\\n)*?\\n(?=(\\w\+?:\\n|### \\d+[\.\\d]+\: |$))",
 			matchCleanedChangelog: "### " + "VERSIONNUMBER" + "(.|\\n)*$",
