742c5b7f9e3cf380fa63f98304fa5a4dc6a75f1b
noud-github <arnoud@yoast.com>
2021-06-24 09:14:37 +0200

    update grunt changlog config

--- config/grunt/task-config/update-changelog-with-latest-pr-texts.js
+++ config/grunt/task-config/update-changelog-with-latest-pr-texts.js
@@ -2,20 +2,19 @@
 /* eslint-disable no-useless-escape */
 /* eslint-disable max-len */
 // Custom task
+// Rem defaultChangelogEntrys: "Other:\n* Includes every change in Yoast SEO core " + "VERSIONNUMBER" + ". See the [core changelog](https://wordpress.org/plugins/wordpress-seo/#developers).\n",
 
 module.exports = {
 	"wpseo-woocommerce": {
 		options: {
-			// Woocommerce header:
-			// ### 15.9: February 23rd, 2021
 			readmeFile: "./README.md",
 			releaseInChangelog: /[#] \d+\.\d+(\.\d+)?\: /g,
 			matchChangelogHeader: /Changelog\n=========\n/ig,
 			newHeadertemplate: "Changelog\n=========\n\n" + "### " + "VERSIONNUMBER" + ": " + "DATESTRING"  + "\n",
-			matchCorrectLines: "### " + "VERSIONNUMBER" + "(.|\\n)*?(?=(### \\d+[\.\\d]+\: |$))",
-			matchCorrectHeader: "### " + "VERSIONNUMBER" + "(.|\\n)*?\\n(?=(\\w\+?:\\n|### \\d+[\.\\d]+\: |$))",
-			matchCleanedChangelog: "### " + "VERSIONNUMBER" + "(.|\\n)*$",
-			replaceCleanedChangelog: "",
+			matchCorrectLines: "### " + "VERSIONNUMBER" + "(.|\\n)*?(?=(### \\d+[\.\\d]+\: |### Earlier versions|$))",
+			matchCorrectHeader: "### " + "VERSIONNUMBER" + "(.|\\n)*?\\n(?=(\\w\+?:\\n|### \\d+[\.\\d]+\: |### Earlier versions|$))",
+			matchCleanedChangelog: "### " + "VERSIONNUMBER" + "(.|\\n)*### Earlier versions|$",
+			replaceCleanedChangelog: "### Earlier versions",
 			defaultChangelogEntries: "",
 			useANewLineAfterHeader: false,
 			useEditDistanceCompare: true,
