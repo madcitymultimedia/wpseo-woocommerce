e72238df734b2aed565e8c952cc9d402358e2ec6
LAPTOP-T1I9C763\leoni <leonidas.milossis@gmail.com>
2021-07-05 11:39:22 +0300

    Fix test

--- tests/classes/woocommerce-dependencies-test.php
+++ tests/classes/woocommerce-dependencies-test.php
@@ -21,7 +21,7 @@ class Yoast_WooCommerce_Dependencies_Test extends TestCase {
 	 */
 	public function test_check_dependencies() {
 		$valid_wp_version        = '5.6';
-		$valid_yoast_seo_version = '16.4';
+		$valid_yoast_seo_version = '16.7';
 
 		$class = Mockery::mock( Yoast_WooCommerce_Dependencies_Double::class )->makePartial();
 
