e3390bccf4c6e5b931f7586996db93f0546d0240
Irene <irene@yoast.com>
2021-10-27 16:19:15 +0200

    Set WC tested up to to 5.8

--- wpseo-woocommerce.php
+++ wpseo-woocommerce.php
@@ -16,7 +16,7 @@
  * Domain Path: /languages/
  *
  * WC requires at least: 3.0
- * WC tested up to: 5.6
+ * WC tested up to: 5.8
  *
  * Copyright 2014-2021 Yoast BV (email: support@yoast.com)
  */
