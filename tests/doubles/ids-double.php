1e4cec7a0e3d53131d1b40c1295fa4c273e33845
Paolo L. Scala <paolo.scala@gmail.com>
2022-05-22 18:47:21 +0200

    Fix linting

--- tests/doubles/ids-double.php
+++ tests/doubles/ids-double.php
@@ -10,23 +10,36 @@ use WPSEO_WooCommerce_Yoast_Ids;
 class Yoast_Ids_Double extends WPSEO_WooCommerce_Yoast_Ids {
 
 	/**
-	 * Gets the variation global identifiers from the options
+	 * Get the variation global identifiers from the options
+	 *
+	 * @param int $post_id  Id of the variation parent post.
+	 * @param int $variation_id  Id of the variation.
+	 *
+	 * @return array The variation global identifiers.
 	 */
 	public function get_variation_values( $post_id, $variation_id ) {
 		return parent::get_variation_values( $post_id, $variation_id );
 	}
 
-
-	public function save_variation_data( $variation_id ) {
-		return parent::save_variation_data( $variation_id );
+	/**
+	 * Validate the variation global identifiers to the options
+	 *
+	 * @param int $variation_id Id of the variation.
+	 *
+	 * @return array Validated variation global identifiers.
+	 */
+	public function validate_variation_data( $variation_id ) {
+		return parent::validate_variation_data( $variation_id );
 	}
 
 	/**
-	 * Displays an input field for an identifier.
+	 * Display an input field for an identifier.
 	 *
-	 * @param string $type  Type of identifier, used for input name.
-	 * @param string $label Label for the identifier input.
-	 * @param string $value Current value of the identifier.
+	 * @param int    $variation_id  Id of the variation.
+	 * @param string $type          Type of identifier, used for input name.
+	 * @param string $label         Label for the identifier input.
+	 * @param string $value         Current value of the identifier.
+	 * @param bool   $is_left       Wether the input text should float to the left of the container div.
 	 *
 	 * @return void
 	 */
