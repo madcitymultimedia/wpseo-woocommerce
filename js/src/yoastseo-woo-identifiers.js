a4b9af156f4583f007bb986ca265990db0b9f0da
Hans-Christiaan <hans-christiaan@yoast.com>
2022-08-09 11:13:22 +0200

    Listen for price changes of simple products and send that over to the analysis.

--- js/src/yoastseo-woo-identifiers.js
+++ js/src/yoastseo-woo-identifiers.js
@@ -145,6 +145,8 @@ function getInitialProductData() {
  * @returns {Object} The product data needed for the SKU and product identifier assessments.
  */
 function getProductData() {
+	const price = document.getElementById( "_regular_price" ).value;
+
 	const sku = document.querySelector( "input#_sku" ).value;
 
 	const gtin8 = document.getElementById( "yoast_identifier_gtin8" ).value;
@@ -156,6 +158,7 @@ function getProductData() {
 
 	const data = {
 		sku,
+		hasPrice: !! price,
 		productIdentifiers: {
 			gtin8,
 			gtin12,
@@ -189,6 +192,7 @@ function enrichDataWithIdentifiers( data ) {
 	const variantsWithPrice = productVariants.filter( variant => variant.hasPrice );
 
 	newData.customData = Object.assign( newData.customData, {
+		hasPrice: product.hasPrice,
 		hasGlobalIdentifier: hasGlobalIdentifier( product ),
 		hasVariants: hasVariants( variantsWithPrice ),
 		doAllVariantsHaveIdentifier: doAllVariantsHaveIdentifier( variantsWithPrice ),
@@ -237,6 +241,10 @@ function registerEventListeners() {
 	// Register event listeners for the global sku input from Woocommerce (non-variation);
 	const globalSkuInput = document.getElementById( "_sku" );
 	globalSkuInput.addEventListener( "change", YoastSEO.app.refresh );
+
+	// Detect changes in the regular price.
+	const globalPriceInput = document.getElementById( "_regular_price" );
+	globalPriceInput.addEventListener( "change", YoastSEO.app.refresh );
 }
 
 /**
