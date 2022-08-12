bc0b89b203f5620d1fc770b4b0435f35135263ef
Manuel <manuel@yoast.com>
2022-08-12 15:01:43 +0200

    Recreate dfbe78fc1a74e6064d6d65b16f711d55f45c8269
    Recreates dfbe78fc1a74e6064d6d65b16f711d55f45c8269, which couldn't be cherry-picked because of previously incorrectly resolved merge conflicts

--- js/src/yoastseo-woo-identifiers.js
+++ js/src/yoastseo-woo-identifiers.js
@@ -253,11 +253,7 @@ function registerEventListeners() {
 	// Since new products do not have a variations element yet, observe the variations tab until it has one.
 	const observer = new MutationObserver( registerVariationsObserver );
 	observer.observe( document.getElementById( "variable_product_options_inner" ), { childList: true } );
-
-	// Listen for changes in the WooCommerce variations (e.g. adding or removing variations).
-	const variationsObserver = new MutationObserver( YoastSEO.app.refresh );
-	variationsObserver.observe( document.querySelector( ".woocommerce_variations" ), { childList: true } );
-
+	
 	// Detect changes in the price inputs and handle them.
 	jQuery( document.body ).on(
 		"change",
