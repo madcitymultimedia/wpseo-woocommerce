531f6d298bbc6c775a42d5b32fca3c6d81ed1163
noud-github <arnoud@yoast.com>
2022-11-09 10:13:28 +0100

    update grunt task to match new changelog format

--- config/grunt/task-config/update-changelog-to-latest.js
+++ config/grunt/task-config/update-changelog-to-latest.js
@@ -8,12 +8,12 @@ module.exports = {
 	"wpseo-woocommerce": {
 		options: {
 			readmeFile: "./README.md",
-			releaseInChangelog: /[#] \d+\.\d+(\.\d+)?\: /g,
+			releaseInChangelog: /[#] \d+\.\d+(\.\d+)?\n\n/g,
 			matchChangelogHeader: /Changelog\n=========\n/ig,
-			newHeadertemplate: "Changelog\n=========\n\n" + "### " + "VERSIONNUMBER" + ": " + "DATESTRING"  + "\n",
-			matchCorrectLines: "### " + "VERSIONNUMBER" + "(.|\\n)*?(?=(### \\d+[\.\\d]+\: |### Earlier versions|$))",
-			matchCorrectHeader: "### " + "VERSIONNUMBER" + "(.|\\n)*?\\n(?=(\\w\+?:\\n|### \\d+[\.\\d]+\: |### Earlier versions|$))",
-			matchCleanedChangelog: "### " + "VERSIONNUMBER" + "(.|\\n)*### Earlier versions|$",
+			newHeadertemplate: "Changelog\n=========\n\n" + "## " + "VERSIONNUMBER" + "\n\nRelease date: " + "DATESTRING"  + "\n",
+			matchCorrectHeader: "## " + "VERSIONNUMBER" + "(.|\\n)*?\\n(?=(\\n#### \\w\+?\\n|## \\d+[\.\\d]+\\n|### Earlier versions|$))",
+			matchCorrectLines: "## " + "VERSIONNUMBER" + "(.|\\n)*?(?=(\\n## \\d+[\.\\d]+|### Earlier versions|$))",
+			matchCleanedChangelog: "## " + "VERSIONNUMBER" + "(.|\\n)*### Earlier versions|$",
 			replaceCleanedChangelog: "### Earlier versions",
 			defaultChangelogEntries: "",
 			useANewLineAfterHeader: false,
