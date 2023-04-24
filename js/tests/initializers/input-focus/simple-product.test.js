8ff02744c8ef86104f8f5501dc539f445589d832
Hans-Christiaan <hans-christiaan@yoast.com>
2023-04-24 11:53:43 +0200

    Added unit tests for SKU field and product identifier focus for simple products.

--- js/tests/initializers/input-focus/simple-product.test.js
+++ js/tests/initializers/input-focus/simple-product.test.js
@@ -1,10 +1,49 @@
 import handleInputFocusForSimpleProducts from "../../../src/initializers/input-focus/simple-product";
+import { openWooCommerceTab, scrollToElement } from "../../../src/initializers/input-focus/helpers";
 
-describe.skip( "input focus for simple products", () => {
+jest.mock( "../../../src/initializers/input-focus/helpers", () => ( {
+	openWooCommerceTab: jest.fn(),
+	scrollToElement: jest.fn(),
+	openVariation: jest.fn(),
+} ) );
+
+/**
+ * Mocks the SKU input element.
+ *
+ * @param {string} sku The SKU.
+ * @returns {HTMLInputElement} The mocked SKU input element.
+ */
+function mockSKUField( sku ) {
+	const element = document.createElement( "input" );
+	element.id = "_sku";
+	element.type = "text";
+	element.value = sku.toString();
+
+	jest.spyOn( element, "focus" );
+
+	return element;
+}
+
+describe( "input focus for simple products", () => {
 	it( "handles the input focus for the SKU assessment", () => {
-		handleInputFocusForSimpleProducts("productSKU" );
+		const mockedSKUField = mockSKUField( "1234-5678" );
+		document.body.appendChild( mockedSKUField );
+
+		handleInputFocusForSimpleProducts( "productSKU" );
+
+		expect( openWooCommerceTab ).toBeCalledWith( ".inventory_tab" );
+		expect( mockedSKUField.focus ).toBeCalled();
+		expect( scrollToElement ).toBeCalledWith( mockedSKUField, { top: -300 } );
 	} );
 	it( "handles the input focus for the product identifiers assessment", () => {
-		handleInputFocusForSimpleProducts( "productIdentifiers" );
+		const productIdentifierTab = document.createElement( "div" );
+		productIdentifierTab.id = "yoast_seo";
+		jest.spyOn( productIdentifierTab, "focus" );
+		document.body.appendChild( productIdentifierTab );
+
+		handleInputFocusForSimpleProducts( "productIdentifier" );
+
+		expect( openWooCommerceTab ).toBeCalledWith( ".yoast_tab_tab" );
+		expect( scrollToElement ).toBeCalledWith( productIdentifierTab, { top: -200 } );
 	} );
 } );
